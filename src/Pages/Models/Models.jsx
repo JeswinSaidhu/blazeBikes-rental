42e45d1c94a1804b96559aa8c8e6cc9707cab3c4
JeswinSaidhu <jeswin.ambati@gmail.com>
2024-01-16 19:15:55 +0530

    Blaze Bike - Rentals: Vehicle models page completed.

--- src/Pages/Models/Models.jsx
+++ src/Pages/Models/Models.jsx
@@ -1,8 +1,408 @@
 import React from "react";
 import "./Models.css";
+import { HeroPages, Footer } from "../../components";
+import { Link } from "react-router-dom";
+import Bike1 from "../../assets/bike-data/re-c350.png";
+import Bike2 from "../../assets/bike-data/re-himalayan.png";
+import Bike3 from "../../assets/bike-data/re-cgt.png";
+import Bike4 from "../../assets/bike-data/re-meteor.png";
+import Bike5 from "../../assets/bike-data/re-scram.png";
+import Bike6 from "../../assets/bike-data/re-hunter.png";
+import Bike7 from "../../assets/bike-data/activa-6g.png";
+import Bike8 from "../../assets/bike-data/activa-125.png";
+import Bike9 from "../../assets/bike-data/tvs-jupiter.png";
+import {
+  IconMotorbike,
+  IconPhone,
+  IconStar,
+  IconStarHalfFilled,
+  IconGasStation,
+} from "@tabler/icons-react";
 
 const Models = () => {
-  return <div></div>;
+  return (
+    <>
+      <section className="models-section">
+        <HeroPages name="Vehicle Models" />
+        <div className="container">
+          <div className="models-div">
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={Bike1} alt="bike_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Classic 350</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹800</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconMotorbike /> &nbsp; RE
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconStarHalfFilled /> &nbsp; 4/5
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconGasStation /> &nbsp; Pertol
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={Bike2} alt="bike_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Himalayan</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹900</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconMotorbike /> &nbsp; RE
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconStarHalfFilled /> &nbsp; 4/5
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconGasStation /> &nbsp; Pertol
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={Bike3} alt="bike_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Continental 650</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹1000</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconMotorbike /> &nbsp; RE
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconStarHalfFilled /> &nbsp; 4/5
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconGasStation /> &nbsp; Pertol
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={Bike4} alt="bike_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Meteor</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹600</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconMotorbike /> &nbsp; RE
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconStarHalfFilled /> &nbsp; 4/5
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconGasStation /> &nbsp; Pertol
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={Bike5} alt="bike_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Scram</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹700</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconMotorbike /> &nbsp; RE
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconStarHalfFilled /> &nbsp; 4/5
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconGasStation /> &nbsp; Pertol
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={Bike6} alt="bike_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Hunter</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹550</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconMotorbike /> &nbsp; RE
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconStarHalfFilled /> &nbsp; 4/5
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconGasStation /> &nbsp; Pertol
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={Bike7} alt="bike_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Activa 6G</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹450</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconMotorbike /> &nbsp; Honda
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconStarHalfFilled /> &nbsp; 4/5
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconGasStation /> &nbsp; Pertol
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={Bike8} alt="bike_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Activa 125</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹400</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconMotorbike /> &nbsp; Honda
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconStarHalfFilled /> &nbsp; 4/5
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconGasStation /> &nbsp; Pertol
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={Bike9} alt="bike_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Jupiter</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹400</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconMotorbike /> &nbsp; TVS Jupiter
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      <IconStarHalfFilled /> &nbsp; 4/5
+                    </span>
+                    <span>
+                      <IconGasStation /> &nbsp; Pertol
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="book-banner">
+          <div className="book-banner__overlay"></div>
+          <div className="container">
+            <div className="text-content">
+              <h2>Book a bike by getting in touch with us</h2>
+              <span>
+                <IconPhone width={40} height={40} />
+                <h3>(123) 456-7869</h3>
+              </span>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </section>
+    </>
+  );
 };
 
 export default Models;
